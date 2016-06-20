27229ebc177206b675d39fedb48d2e7bf895cc43
Mahed Ali <Mahed Ali@172.16.105.229>
2016-06-20 14:08:21 +0500

    all is well

---
+++ src/BankSimulation.java
@@ -0,0 +1,22 @@
+
+public class BankSimulation {
+	/**
+	 * This is a main class.
+	 * In this be make objects of other classes bankclass,person,checkingaccount & savingaccount classes. 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/**
+		 * Four Objects are made
+		 */
+		
+		// TODO Auto-generated method stub
+Bank bn=new Bank();
+Person pr=new Person();
+CheckingAccount ca=new CheckingAccount();
+SavingAccount sa=new SavingAccount();
+
+	}
+
+}
